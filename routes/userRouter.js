5368a3b6da13116c1a631537104d536fdfee12bf
IagodeOliveira <iago.idom@gmail.com>
2021-07-28 12:28:53 -0300

    no options

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,12 +4,12 @@ const userController = require('../controller/userController');
 const auth = require('../controller/auth');
 const cors = require('cors');
 
-const options = {
-    // origin: 'http://localhost:5000'
-    origin: 'https://chat-teste1.herokuapp.com/'
-}
+// const options = {
+//     // origin: 'http://localhost:5000'
+//     origin: 'https://chat-teste1.herokuapp.com/'
+// }
 
-router.use(cors(options));
+// router.use(cors(options));
 
 router.use(cors());
 
