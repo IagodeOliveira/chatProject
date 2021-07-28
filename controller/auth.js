3a66a7eaf603340c511876516a0432c8870e9974
IagodeOliveira <iago.idom@gmail.com>
2021-07-28 15:46:10 -0300

    re2s

--- controller/auth.js
+++ controller/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next) {
     const token = req.header('authorization-token');
     if(!token || token === 'null') {
-        return res.status(401).send('Restricted Area');
+        //return res.status(401).send('Restricted Area');
     }  
 
     try {
@@ -11,6 +11,6 @@ module.exports = function(req, res, next) {
         req.user = userVerified;
         next();
     } catch(err) {
-        return res.status(401).send('Restricted Area');
+        //return res.status(401).send('Restricted Area');
     }
 }
